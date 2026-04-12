611b03a1dd9b6d49a7b877e1afe278ccf3008e1a
amitanr <a8124141@gsuite.si.aoyama.ac.jp>
2026-04-12 11:38:08 +0900

    initial commit

---
+++ jsonfile0alt.js
@@ -0,0 +1,14 @@
+const item_origin = {
+    "item0": {
+        "attribute1": 1,
+        "attribute2": "Hello",
+        "attribute3": true,
+        "array": ["1", "Hello", "TRUE", "レンタルサーバー"]
+    },
+    "item1": {
+        "attribute1": 2,
+        "attribute2": "World",
+        "attribute3": false,
+        "array": ["2", "World", "FALSE", "動作確認"]
+  }
+};
