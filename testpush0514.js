aefa5570da33be4ab152de3d3826200db0f2b958
amitanr <a8124141@gsuite.si.aoyama.ac.jp>
2026-05-14 13:05:19 +0900

    追加0514

---
+++ testpush0514.js
@@ -0,0 +1,15 @@
+const item_origin = {
+    "item0": {
+        "attribute1": 1,
+        "attribute2": "Hello",
+        "attribute3": true,
+        "array": ["1", "Hello", "TRUE", "git"]
+    },
+    "item1": {
+        "attribute1": 2,
+        "attribute2": "World",
+        "attribute3": false,
+        "array": ["2", "World", "FALSE", "動作確認"]
+  }
+};
+//テスト
